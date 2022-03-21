0a984cffc2613d5875b6d5125e4004f2db628f1b
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2022-03-21 19:53:19 +0300

    fix: исправление после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,41 @@
-const popupElement = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+const popupElement = document.querySelector(".popup");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
 const ESC_KEY = "Escape";
 
-let formElement = document.querySelector('.popup-form');
-let nameInput = document.querySelector('.popup-form__item_el_name');
-let jobInput = document.querySelector('.popup-form__item_el_job');
-let nameValue = document.querySelector('.profile__title');
-let jobValue = document.querySelector('.profile__text');
-
+const formElement = document.querySelector(".popup-form");
+const nameInput = document.querySelector(".popup-form__item_el_name");
+const jobInput = document.querySelector(".popup-form__item_el_job");
+const nameValue = document.querySelector(".profile__title");
+const jobValue = document.querySelector(".profile__text");
 
 function openPopup() {
-    popupElement.classList.add('popup_opened');
-    document.addEventListener('keyup', onDocumentKeyUp);
-    nameInput.value = nameValue.textContent;
-    jobInput.value = jobValue.textContent;
+  popupElement.classList.add("popup_opened");
+  document.addEventListener("keyup", onDocumentKeyUp);
+  nameInput.value = nameValue.textContent;
+  jobInput.value = jobValue.textContent;
 }
 
 function closePopup() {
-    popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keyup', onDocumentKeyUp);
+  popupElement.classList.remove("popup_opened");
+  document.removeEventListener("keyup", onDocumentKeyUp);
 }
 
 function onDocumentKeyUp(event) {
-    if (event.key === ESC_KEY) {
-        closePopup();
-    }
-}
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameValue.textContent = nameInput.value;
-    jobValue.textContent = jobInput.value;
+  if (event.key === ESC_KEY) {
     closePopup();
+  }
 }
 
-editButton.addEventListener('click', openPopup);
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  nameValue.textContent = nameInput.value;
+  jobValue.textContent = jobInput.value;
+  closePopup();
+}
 
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener("click", openPopup);
 
-formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener("click", closePopup);
 
+formElement.addEventListener("submit", handleProfileFormSubmit);
